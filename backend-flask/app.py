656aee043574433e3f0624e6fd11595a2cd6d40f
unspacy <aex.spear@gmail.com>
2023-02-19 12:48:17 +0000

    uncomment line 3 cors module

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
