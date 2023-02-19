4f0da353dc4905796efdf7620802813725ce8ebc
unspacy <aex.spear@gmail.com>
2023-02-19 12:44:31 +0000

    Initial Commit update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,8 +14,8 @@ from services.create_message import *
 from services.show_activity import *
 
 app = Flask(__name__)
-frontend = os.getenv('FRONTEND_URL')
-backend = os.getenv('BACKEND_URL')
+frontend = os.getenv('FRONTEND_URL="*"')
+backend = os.getenv('BACKEND_URL="*"')
 origins = [frontend, backend]
 cors = CORS(
   app, 
