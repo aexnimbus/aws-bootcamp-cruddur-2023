d142d6c721ff1511412045479a11b520dcad290f
unspacy <aex.spear@gmail.com>
2023-02-21 07:03:41 +0000

    Debugging flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS, cross_origin
+flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
