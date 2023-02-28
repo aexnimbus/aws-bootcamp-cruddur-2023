90bce67c911a55b1e1180bdcf898ab6679c14762
unspacy <aex.spear@gmail.com>
2023-02-28 12:59:44 +0000

    adding aws-xray-sdk code in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,15 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+# aws x-ray-sdk
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+# end of aws-xray-sdk
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
