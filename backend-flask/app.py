343190ad73dcdb16238a64f3bb2a77eb2d899509
unspacy <aex.spear@gmail.com>
2023-03-01 13:36:56 +0000

    Renaming service from crudder to backend-flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,7 +22,7 @@ from services.show_activity import *
 
 # x-ray 
 xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 # end of aws-xray-sdk
 
