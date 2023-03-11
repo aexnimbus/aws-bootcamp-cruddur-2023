530a11474eeaa487273394f0cce733cc5990217d
unspacy <aex.spear@gmail.com>
2023-03-11 12:16:25 +0000

    commenting rollbar to fix the issue

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -104,30 +104,30 @@ cors = CORS(
 
 #start rollbar snippet 
 
-rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
-def init_rollbar():
-    """init rollbar module"""
-    rollbar.init(
+#rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+#@app.before_first_request
+#def init_rollbar():
+ #   """init rollbar module"""
+ #   rollbar.init(
         # access token
-        '0a410a8c4fb54911b068fc13c82bd672',
+ #       '0a410a8c4fb54911b068fc13c82bd672',
         # environment name
-        'production',
+ #       'production',
         # server root directory, makes tracebacks prettier
-        root=os.path.dirname(os.path.realpath(__file__)),
+  #      root=os.path.dirname(os.path.realpath(__file__)),
         # flask already sets up logging
-        allow_logging_basic_config=False)
+  #      allow_logging_basic_config=False)
 
     # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+  #  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 # end of rollbar snippet
 
 # rollbar /
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+#@app.route('/rollbar/test')
+#def rollbar_test():
+   # rollbar.report_message('Hello World!', 'warning')
+   # return "Hello World!"
 # end of rollbar /
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
