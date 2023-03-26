8a8b019ab9dbb0e58496d798b3d202896d8c83ee
unspacy <aex.spear@gmail.com>
2023-03-26 16:15:27 +0000

    debugging backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -82,21 +82,28 @@ app = Flask(__name__)
 #FlaskInstrumentor().instrument_app(app)
 #RequestsInstrumentor().instrument()
 # Still a Honeycomb
-
-# omenking code 
-
-
 frontend = os.getenv('FRONTEND_URL="*"')
 backend = os.getenv('BACKEND_URL="*"')
 origins = [frontend, backend]
+## new code omenking
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
+## end of new code
+## old code 
+#cors = CORS(
 
+#  app, 
+#  resources={r"/api/*": {"origins": origins}},
+#  expose_headers="location,link",
+#  allow_headers="content-type,if-modified-since",
+#  methods="OPTIONS,GET,HEAD,POST"
+#)
+## end of old code 
 #cloudwatch endpoint 
 #@app.after_request
 #def after_request(response):
