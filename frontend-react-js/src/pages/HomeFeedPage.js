ca4858d5ce10cda11c9dd92b8d41916bde55c227
unspacy <aex.spear@gmail.com>
2023-04-08 13:14:39 +0000

    insert code with comment inside HomeFeedPae.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -7,6 +7,40 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
 
+// beg of the code Week3
+import { Auth } from 'aws-amplify';
+
+// set a state
+const [user, setUser] = React.useState(null);
+
+// check if we are authenicated
+const checkAuth = async () => {
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+  }).then((cognito_user) => {
+      setUser({
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
+      })
+  })
+  .catch((err) => console.log(err));
+};
+
+// check when the page loads if we are authenicated
+React.useEffect(()=>{
+  loadData();
+  checkAuth();
+}, [])
+
+// this the end of the code 
+
 js
 headers: {
   Authorization: `Bearer ${localStorage.getItem("access_token")}`
